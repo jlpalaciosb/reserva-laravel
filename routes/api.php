4ca83872c793316562a195710158c347a3c8c7c3
Jose Palacios <joselupalaciosbo@gmail.com>
2022-05-08 20:31:02 -0400

    commit middleware

--- routes/api.php
+++ routes/api.php
@@ -26,28 +26,31 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// ruta para crear token
-Route::post('/tokens/create', function (Request $request) {
-    $request->validate([
-        'username' => 'required',
-        'password' => 'required',
-        'device' => 'required',
-    ]);
-    $usu = Usuario::where('username', $request->username)->first();
-    if (! $usu || ! Hash::check($request->password, $usu->password)) {
-        throw ValidationException::withMessages([
-            'username' => ['Las credenciales proveídas son incorrectas.'],
+// grupo de rutas SIN autenticacion
+Route::middleware(['allow.origin.all'])->group(function () {
+    // ruta para crear token
+    Route::post('/tokens/create', function (Request $request) {
+        $request->validate([
+            'username' => 'required',
+            'password' => 'required',
+            'device' => 'required',
         ]);
-    }
-    return response()->json([
-        'usuario' => $usu,
-        'token' => $usu->createToken($request->device)->plainTextToken,
-    ]);
+        $usu = Usuario::where('username', $request->username)->first();
+        if (! $usu || ! Hash::check($request->password, $usu->password)) {
+            throw ValidationException::withMessages([
+                'username' => ['Las credenciales proveídas son incorrectas.'],
+            ]);
+        }
+        return response()->json([
+            'usuario' => $usu,
+            'token' => $usu->createToken($request->device)->plainTextToken,
+        ]);
+    });
+    // ruta para crear usuario
+    Route::post('/registro', [UsuarioController::class, 'store']);
 });
-// ruta para crear usuario
-Route::post('/registro', [UsuarioController::class, 'store']);
 
-// en este grupo con middleware van todas las rutas autenticadas
+// grupo de rutas CON autenticacion
 Route::middleware(['auth:sanctum', 'allow.origin.all'])->group(function () {
     Route::get('/usuario', function (Request $request) {
         return $request->user();
