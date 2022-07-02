a91da15d310446629c892780c93233ca7413b4cf
Jose Palacios <joselupalaciosbo@gmail.com>
2022-07-02 15:07:30 -0400

    log

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,7 +27,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if (App::environment('production')) {
+            Log::info('con esquema https');
             URL::forceScheme('https');
+        } else {
+            Log::info('sin esquema https');
         }
     }
 }
